a7e6ca024ca482fa15f7065f76e3b80eef6bd683
Nuno Araujo <nunobaraujo@hotmail.com>
2020-04-22 23:32:28 -0300

    board model bugfix

--- src/app/Models/board.ts
+++ src/app/Models/board.ts
@@ -1,9 +1,7 @@
 import { BoardTile } from './boardTile';
-import { WordPlay } from './wordPlay';
 
 export class Board {    
     rows:number;
     columns: number;
-    tiles:BoardTile[] ;
-    words:WordPlay[];    
+    tiles:BoardTile[];
 }
