16a2497de6b714d2a8dcdf1756d0f728168e5574
nuno.salgado <nsalgado@cendyn.com>
2022-04-29 17:40:08 -0300

    Added CustomValidator for password confirmation

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { first } from 'rxjs/operators';
 import { UserService } from 'src/app/Services/user.service';
+import { CustomValidators } from 'src/app/Providers/CustomValidators';
 
 @Component({
   selector: 'app-signup',
@@ -10,15 +11,28 @@ import { UserService } from 'src/app/Services/user.service';
   styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent implements OnInit {
-  loginForm: FormGroup;
   loading = false;
   submitted = false;
   returnUrl: string;
   error = '';
   created:string=null;
 
+  loginForm = new FormGroup(
+    {
+      username: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(8)
+      ]),
+      confirmPassword: new FormControl('', [Validators.required])
+    },
+
+    CustomValidators.mustMatch('password', 'confirmPassword') // insert here
+  );
+
+
   constructor(
-    private formBuilder: FormBuilder,
     private route: ActivatedRoute,
     private router: Router,
     private userService:UserService)
@@ -27,14 +41,8 @@ export class SignupComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.loginForm = this.formBuilder.group({
-      username: ['', [Validators.required,Validators.minLength(6)]],
-      email: ['', Validators.required],
-      password: ['', [Validators.required,Validators.minLength(8)]]
-  });
-
-  // get return url from route parameters or default to '/'
-  this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+    // get return url from route parameters or default to '/'
+    this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
   }
 
   get f() { return this.loginForm.controls; }
@@ -44,7 +52,7 @@ export class SignupComponent implements OnInit {
 
     // stop here if form is invalid
     if (this.loginForm.invalid) {
-      console.log('object :>> ', this.loginForm.errors);
+      console.log('Invalid: object :>> ', this.loginForm.errors);
         return;
     }
 
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://46.101.0.179:5005'
+  apiUrl: 'http://146.190.227.232:5005'
 };
